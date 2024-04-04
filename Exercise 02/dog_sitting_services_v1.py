20e03cb31da975024e7fd88c1a3e8cc421b4f971
dychoe2008 <dychoe2008@gmail.com>
2024-04-04 10:17:01 +0900

    Update dog_sitting_services_v1.py

---
+++ Exercise 02/dog_sitting_services_v1.py
@@ -0,0 +1,27 @@
+"""Dog sitting service
+Print the welcome screen and ask the user for an input from 1-5
+By Daniel Choe
+"""
+
+
+
+
+
+print("-----------------------------------------------------------------------")
+print("Welcome to DogsRus")
+print("What would you like to do? Please choose one of the items below")
+print("-----------------------------------------------------------------------")
+print()
+print("1 = Drop off a dog")
+print("2 = Pick up a dog")
+print("3 = Calculate cost")
+print("4 = Print roll")
+print("5 = Exit the system")
+print()
+choice=int(input("Enter your choice (number between 1 and 5): "))
+print()
+
+
+
+
+
