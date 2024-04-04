9b3f0a8f647a7ece1146f0cd173fd270caebf9a8
dychoe2008 <dychoe2008@gmail.com>
2024-04-04 10:22:42 +0900

    Update dog_sitting_services_v1.py

--- Exercise 02/dog_sitting_services_v1.py
+++ Exercise 02/dog_sitting_services_v1.py
@@ -3,10 +3,20 @@ Print the welcome screen and ask the user for an input from 1-5
 By Daniel Choe
 """
 
+def int_checker(question):
+    """Checks if the input was a null or string repeats until it receives a int."""
+    error = "\n Sorry, you must enter an integer that's from 1-5\n"
+    num = ""
+    while not num:
+        try:
+            num = int(input(question))
+            return num
+        except ValueError:
+            print(error)
 
 
 
-
+# Main Routine
 print("-----------------------------------------------------------------------")
 print("Welcome to DogsRus")
 print("What would you like to do? Please choose one of the items below")
@@ -18,7 +28,7 @@ print("3 = Calculate cost")
 print("4 = Print roll")
 print("5 = Exit the system")
 print()
-choice=int(input("Enter your choice (number between 1 and 5): "))
+choice=int_checker("Enter your choice (number between 1 and 5): ")
 print()
 
 
