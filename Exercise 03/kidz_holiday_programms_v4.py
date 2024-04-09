61af8ea3e7af646768115696502089ee26533327
dychoe2008 <dychoe2008@gmail.com>
2024-04-09 22:15:57 +0900

    Update kidz_holiday_programms_v4.py

--- Exercise 03/kidz_holiday_programms_v4.py
+++ Exercise 03/kidz_holiday_programms_v4.py
@@ -4,7 +4,7 @@ and average age of the children in each holiday programme.
 By Daniel
 """
 # pylint: disable = c0103
-def str_checker(string, question_01):
+def str_check(string, question_01):
     """Checks if the everything in the input is in the alpahbet"""
     invaild = "\n Sorry, you must enter F,A or X\n"
     while string.isalpha() is False:
@@ -13,8 +13,8 @@ def str_checker(string, question_01):
     return string
 
 def int_checker(question_02):
-    """Checks if the input was a null or string repeats until it receives a int."""
-    invaild = "\n Sorry, you must enter an integer that's 5-15 and it must be a whole number\n"
+    """Checks the input was a null or string repeats until receives a int."""
+    invaild = "\n You must enter an integer that's 5-15\n"
     num = ""
     while not num:
         try:
@@ -34,20 +34,20 @@ total_C_A = 0
 
 while keep_running:
     repeat = "Go"
-    print("-----------------------------------------------------------------------")
+    print("------------------------------------------------------------------")
     print("******** Welcome to Holiday Kidz Programme! ********")
     print("What would you like to do? Please choose one of the items below")
-    print("-----------------------------------------------------------------------")
+    print("------------------------------------------------------------------")
     print()
     print("F = Sign your child up for 'Fun in the sun'")
     print("A = Sign your child up for 'Active Kidz'")
     print("X = End and List of children and their age in each programme")
     print()
-    choice = input("Enter your choice (enter F,A or X): ")
-    choice_check = str_checker(choice, "Enter your choice (enter F,A or X): ").title()
+    choice = input("Enter your choice (F,A or X): ")
+    choice_c = str_check(choice, "Enter your choice (F,A or X): ").title()
 
     while repeat != "Stop":
-        if choice_check == "X":
+        if choice_c == "X":
             repeat = "Stop"
             keep_running = False
             total_C_F = len(C_A_list_F)
@@ -56,35 +56,35 @@ while keep_running:
             if total_C_F == 0 and total_C_A == 0:
                 print("We have no children signed up for either activities")
             elif total_C_F == 0:
-                average_A_A = sum(C_A_list_A) // len(C_A_list_A)
+                a_A_A = sum(C_A_list_A) // len(C_A_list_A)
                 print("We have no children signed up for 'Fun in the Sun'")
-                print(f"A:\nTotal Children = {total_C_A}\nAverage age = {round(average_A_A)}")
+                print(f"A:\nChildren = {total_C_A}\nAverage = {round(a_A_A)}")
             elif total_C_A == 0:
-                average_A_F = sum(C_A_list_F) // len(C_A_list_F)
+                a_A_F = sum(C_A_list_F) // len(C_A_list_F)
                 print("We have no children signed up for 'Active Kidz'")
-                print(f"F:\nTotal Children = {total_C_F}\nAverage age = {round(average_A_F)}")
+                print(f"F:\nChildren = {total_C_F}\nAverage = {round(a_A_F)}")
             else:
-                average_A_F = sum(C_A_list_F) // len(C_A_list_F)
-                average_A_A = sum(C_A_list_A) // len(C_A_list_A)
-                print(f"F:\nTotal Children = {total_C_F}\nAverage age = {round(average_A_F)}")
-                print(f"A:\nTotal Children = {total_C_A}\nAverage age = {round(average_A_A)}")
+                a_A_F = sum(C_A_list_F) // len(C_A_list_F)
+                a_A_A = sum(C_A_list_A) // len(C_A_list_A)
+                print(f"F:\nChildren = {total_C_F}\nAverage = {round(a_A_F)}")
+                print(f"A:\nChildren = {total_C_A}\nAverage = {round(a_A_A)}")
                 print("Thank you for signing up to our programms!")
 
-        elif choice_check == "F":
+        elif choice_c == "F":
             repeat = "Stop"
             child_age = int_checker("Enter the age your child (5-15 only): ")
             while child_age < 5 or child_age > 15:
                 print(ERROR_02)
-                child_age = int_checker("Enter the age your child (5-15 only): ")
+                child_age=int_checker("Enter the age your child (5-15 only): ")
             C_A_list_F.append(child_age)
-        elif choice_check == "A":
+        elif choice_c == "A":
             repeat = "Stop"
             child_age = int_checker("Enter the age your child (5-15 only): ")
             while child_age < 5 or child_age > 15:
                 print(ERROR_02)
-                child_age = int_checker("Enter the age your child (5-15 only): ")
+                child_age=int_checker("Enter the age your child (5-15 only): ")
             C_A_list_A.append(child_age)
         else:
             print(ERROR_01)
-            choice = input("Enter your choice (enter F,A or X): ")
-            choice_check = str_checker(choice, "Enter your choice (enter F,A or X): ").title()
+            choice = input("Enter your choice (F,A or X): ")
+            choice_c=str_check(choice,"Enter your choice (F,A or X): ").title()
